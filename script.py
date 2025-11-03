d774bf46bc9af6bb66dddceb2518ac3558210079
MilleniumBlitz <xavier.lafficher@gmail.com>
2025-11-03 22:36:12 +0100

    Envoyer la liste des saints du jour vers un chat telegram

--- script.py
+++ script.py
@@ -22,8 +22,14 @@ class Saint:
 # Chargement des variables d'environnement contenues dans le fichier .env
 load_dotenv()
 
-# Récupération du token du bot
+# Récupération de l'URL du webhook discord
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+# Récupération du token du bot Telegram
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# Récupération de l'identifiant du chat Telegram
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 # Configuration du logger
 logger = logging.getLogger(__name__)
@@ -34,6 +40,7 @@ logging.basicConfig(
 )
 
 def recuperer_page_saints_du_mois(nom_du_mois) -> BeautifulSoup:
+
     # Construction de l'URL permettant de récuperer les Saints du mois
     url = f"{BASE_URL}saints-{unidecode(nom_du_mois)}.html"
 
@@ -107,31 +114,51 @@ def recuperer_saints_du_jour() -> list[Saint]:
     logger.info(f"Récupération des Saints du jour terminé : {len(saints_du_jour)} récupéré(s)")
     return saints_du_jour
 
+def envoyer_saint_du_jour_discord(saints_du_jour):
+
+    if len(saints_du_jour) == 0:
+        DiscordWebhook(url=WEBHOOK_URL, content="Oups ! 0 Saint trouvé aujourd'hui ! Mais que fais le X solide et crémeux à la fois ?").execute()
+
+    # Récupération des Saints du jour
+    for saint in saints_du_jour:
+        # Découpage du texte en bloc de 2000 caractères (limite d'envoi de Discord)
+        paragraphes = wrap(saint.description, 2000)
+
+        # Pour chacun des blocs de 2000 caractères, les envoyer
+        for paragraphe in paragraphes:
+            # Création et execution d'un webhook contenant le paragraphe 
+            DiscordWebhook(url=WEBHOOK_URL, content=paragraphe).execute()
+
+        # Si le Saint du jour possède une image
+        if saint.url_image is not None:
+            
+            # Téléchargement de l'image du Saint
+            reponse = requests.get(saint.url_image)
+
+            # Si le téléchargement est OK
+            if reponse.ok:
+
+                # Création et execution d'un webhook contenant l'image
+                webhook = DiscordWebhook(url=WEBHOOK_URL)
+                webhook.add_file(reponse.content, saint.nom + ".jpg")
+                webhook.execute()
+
+def envoyer_saint_du_jour_telegram(saints_du_jour: list[Saint]):
+
+    base_url_telegram = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/"
+    url_message = base_url_telegram + "sendMessage"
+    url_photo = base_url_telegram + "sendPhoto"
+
+    for saint in saints_du_jour:
+
+        if saint.url_image is not None:
+            payload_photo = {"chat_id": TELEGRAM_CHAT_ID, "photo": saint.url_image}
+            requests.post(url_photo, json=payload_photo)
+        
+        payload_text = {"chat_id": TELEGRAM_CHAT_ID, "text": saint.description}
+        requests.post(url_message, json=payload_text)
+
 saints_du_jour = recuperer_saints_du_jour()
 
-if len(saints_du_jour) == 0:
-    DiscordWebhook(url=WEBHOOK_URL, content="Oups ! 0 Saint trouvé aujourd'hui ! Mais que fais le X solide et crémeux à la fois ?").execute()
-
-# Récupération des Saints du jour
-for saint in saints_du_jour:
-     # Découpage du texte en bloc de 2000 caractères (limite d'envoi de Discord)
-    paragraphes = wrap(saint.description, 2000)
-
-    # Pour chacun des blocs de 2000 caractères, les envoyer
-    for paragraphe in paragraphes:
-        # Création et execution d'un webhook contenant le paragraphe 
-        DiscordWebhook(url=WEBHOOK_URL, content=paragraphe).execute()
-
-    # Si le Saint du jour possède une image
-    if saint.url_image is not None:
-        
-        # Téléchargement de l'image du Saint
-        reponse = requests.get(saint.url_image)
-
-        # Si le téléchargement est OK
-        if reponse.ok:
-
-            # Création et execution d'un webhook contenant l'image
-            webhook = DiscordWebhook(url=WEBHOOK_URL)
-            webhook.add_file(reponse.content, saint.nom + ".jpg")
-            webhook.execute()
+envoyer_saint_du_jour_discord(saints_du_jour)
+envoyer_saint_du_jour_telegram(saints_du_jour)

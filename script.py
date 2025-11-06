00a3401de7f103c826b56679cccc412b517fc1fc
MilleniumBlitz <xavier.lafficher@gmail.com>
2025-11-06 21:52:57 +0100

    Utilisation de l'API SaintDuJour

--- script.py
+++ script.py
@@ -1,13 +1,9 @@
 from discord_webhook import DiscordWebhook
-import datetime
 from dotenv import load_dotenv
 import os
 import requests
 import logging
-from bs4 import BeautifulSoup
 from textwrap import wrap
-from unidecode import unidecode
-from babel.dates import format_date
 
 BASE_URL = "https://liguesaintamedee.ch/"
 
@@ -22,8 +18,10 @@ class Saint:
 # Chargement des variables d'environnement contenues dans le fichier .env
 load_dotenv()
 
+API_URL = os.getenv("API_URL")
+
 # Récupération de l'URL du webhook discord
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
 # Récupération du token du bot Telegram
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -39,108 +37,40 @@ logging.basicConfig(
     filename="saint_du_jour.log"
 )
 
-def recuperer_page_saints_du_mois(nom_du_mois) -> BeautifulSoup:
-
-    # Construction de l'URL permettant de récuperer les Saints du mois
-    url = f"{BASE_URL}saints-{unidecode(nom_du_mois)}.html"
-
-    # Récupération de la page des Saints du mois
-    reponse = requests.get(url, verify=False)
-
-    if not reponse.ok:
-        raise RuntimeError(f"Erreur HTTP {reponse.status_code} en récupérant {url}")
-
-    return BeautifulSoup(reponse.content, "html.parser")
-
-def recuperer_url_image_saint_du_jour(page_web, nom_saint) -> str:
-    return page_web.find(alt=nom_saint, name="img")
-
 def recuperer_saints_du_jour() -> list[Saint]:
 
     logger.info('Lancement de la récupération des Saints du jour')
 
-    saints_du_jour = []
-
-    date_jour = datetime.datetime.now()
-    nom_du_mois = format_date(date_jour, format="MMMM", locale="fr_FR")
-    
-    try:
-        html_parse = recuperer_page_saints_du_mois(nom_du_mois)
-    except Exception as e:
-        logger.error(f"Impossible de récupérer la page des saints : {e}")
-        return []  # le script continue, mais vide
-
-    numero_jour = '{dt.day}'.format(dt = date_jour)
-    if numero_jour == "1":
-        numero_jour = "1er"
-
-    resultats_recherche_balises_saint = html_parse.find_all(string = f"{numero_jour} {nom_du_mois}", name="b")
-    
-    # Je parcours l'ensembe des balises
-    for balise_saint in resultats_recherche_balises_saint:
-
-        # Récupération de la date de ce Saint
-        date_saint = balise_saint.contents[0]
-
-        nom_saint = None
-        description_saint = ""
-
-        for element_html in date_saint.next_elements:
-            element_texte = element_html.text
-
-            # Si on arrive sur le texte " Retour en haut ", c'est qu'on a finit de lire la description de ce saint, on peut passer au suivant
-            if element_texte.strip() == "Retour en haut":
-                break
-            if element_texte.strip() != '':
-                if element_html.name == "u":
-                    nom_saint = element_html.text
-                    logger.info(f"Saint récupéré : {nom_saint}")
-                elif element_html.name != "i":
-                    description_saint += element_texte
-
-        saint_du_jour = Saint(nom_saint, description_saint)
-
-        # Recherche de l'image du Saint
-        image = recuperer_url_image_saint_du_jour(html_parse, nom_saint)
-
-        # Si elle existe ...
-        if image:
-
-            # L'ajouter au sein du jour
-            saint_du_jour.url_image = BASE_URL + image.attrs['src']
-
-        saints_du_jour.append(saint_du_jour)
-
-    logger.info(f"Récupération des Saints du jour terminé : {len(saints_du_jour)} récupéré(s)")
-    return saints_du_jour
+    r = requests.get(API_URL)
+    return r.json()
 
 def envoyer_saint_du_jour_discord(saints_du_jour):
 
     if len(saints_du_jour) == 0:
-        DiscordWebhook(url=WEBHOOK_URL, content="Oups ! 0 Saint trouvé aujourd'hui ! Mais que fais le X solide et crémeux à la fois ?").execute()
+        DiscordWebhook(url=DISCORD_WEBHOOK_URL, content="Oups ! 0 Saint trouvé aujourd'hui ! Mais que fais le X solide et crémeux à la fois ?").execute()
 
     # Récupération des Saints du jour
     for saint in saints_du_jour:
         # Découpage du texte en bloc de 2000 caractères (limite d'envoi de Discord)
-        paragraphes = wrap(saint.description, 2000)
+        paragraphes = wrap(saint['description'], 2000)
 
         # Pour chacun des blocs de 2000 caractères, les envoyer
         for paragraphe in paragraphes:
             # Création et execution d'un webhook contenant le paragraphe 
-            DiscordWebhook(url=WEBHOOK_URL, content=paragraphe).execute()
+            DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=paragraphe).execute()
 
         # Si le Saint du jour possède une image
-        if saint.url_image is not None:
+        if saint['image'] is not None:
             
             # Téléchargement de l'image du Saint
-            reponse = requests.get(saint.url_image)
+            reponse = requests.get(saint['image'])
 
             # Si le téléchargement est OK
             if reponse.ok:
 
                 # Création et execution d'un webhook contenant l'image
-                webhook = DiscordWebhook(url=WEBHOOK_URL)
-                webhook.add_file(reponse.content, saint.nom + ".jpg")
+                webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL)
+                webhook.add_file(reponse.content, saint['nom'] + ".jpg")
                 webhook.execute()
 
 def envoyer_saint_du_jour_telegram(saints_du_jour: list[Saint]):
@@ -161,4 +91,4 @@ def envoyer_saint_du_jour_telegram(saints_du_jour: list[Saint]):
 saints_du_jour = recuperer_saints_du_jour()
 
 envoyer_saint_du_jour_discord(saints_du_jour)
-envoyer_saint_du_jour_telegram(saints_du_jour)
+#envoyer_saint_du_jour_telegram(saints_du_jour)
